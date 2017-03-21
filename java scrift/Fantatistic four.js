da4f9a8d03bac3ae6252c6f30edc12e60c3b54f2
Manoj B <manoj@synnov.com>
2017-03-21 20:45:23 +0530

    pushed first commit

---
+++ java scrift/Fantatistic four.js
@@ -0,0 +1,12 @@
+$('.owl-carousel').owlCarousel({
+		    loop:false,
+		    margin:0,
+		    nav:false,
+		    dots: false,
+		    responsive:{
+		        0:{
+		            items:1
+		        },
+		        
+		    }
+		});
